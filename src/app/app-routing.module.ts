80b57a4b19df6a4c698beebe39903873d74149d0
vdvman1 <vdvman88@gmail.com>
2020-04-26 17:43:06 +1000

    Statically load home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './routes/home/home.component';
+
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/home' },
-  { path: 'home', loadChildren: () => import('./routes/home/home.module').then(m => m.HomeModule) }
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  // { path: 'home', loadChildren: () => import('./routes/home/home.module').then(m => m.HomeModule) }
 ];
 
 @NgModule({
