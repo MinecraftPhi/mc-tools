7f76a4975fbb10078e1b848a6841fe3e5e521327
vdvman1 <vdvman88@gmail.com>
2020-05-01 19:33:11 +1000

    Move version service into its own folder

--- src/app/components/version/version.component.ts
+++ src/app/components/version/version.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { VersionService, FetchingStatus } from 'src/app/services/version.service';
+import { VersionService, FetchingStatus } from 'src/app/services/version/version.service';
 
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
